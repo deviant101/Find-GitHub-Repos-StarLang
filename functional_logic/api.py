2e4bccac0f1c7aa407d393ff66c8861b4ed8d7ad
deviant <115878714+deviant101@users.noreply.github.com>
2024-08-08 00:33:52 +0500

    Exception for API response created

--- functional_logic/api.py
+++ functional_logic/api.py
@@ -1,5 +1,6 @@
 import requests
 from models import GitHubRepo
+from exceptions import GitHubApiException
 
 GH_SEARCH_API_URL = "https://api.github.com/search/repositories"
 
@@ -8,17 +9,17 @@ def most_stars_repos(languages, min_stars=100000, sort="stars", order="ascending
     query = create_query(languages=languages, min_stars=min_stars)
     parameters = {"q":query, "sort":sort, "order":order}
     response = requests.get(GH_SEARCH_API_URL,params=parameters)
-    # print(response.status_code)
+    
+    if response.status_code != 200:
+        raise GitHubApiException(response.status_code)
 
     json_response = response.json()
     # print(json_response["items"][0].keys())   # to filter & find useful keys of repo
     items = json_response["items"]              # returning list of dictionaries
 
     return [GitHubRepo({item["name"]}, {item["language"]}, {item["stargazers_count"]}) for item in items]
-    
 
-
-def create_query(languages, min_stars):     # creating query string
+def create_query(languages, min_stars):
     
     query = " ".join( f"language:{lang}" for lang in languages )
     query += f" stars:>{min_stars}"
--- functional_logic/models.py
+++ functional_logic/models.py
@@ -6,7 +6,7 @@ class GitHubRepo:
         self.stars = stars
     
     def __str__(self) -> str:
-        return f"{self.name} is a {self.language} repo with {self.stars}."
+        return f"{self.name} is a {self.language} repo with {self.stars}"
     
     def __repr__(self) -> str:
         return f"GitHubRepo({self.name}, {self.language}, {self.stars})"
